c089687c69ff8af3e7efdbefce1056168a9a80cd
le.nv-at-861189208020 <le.nv@platinumtechnology.vn>
2024-05-17 08:10:25 +0700

    no message

---
+++ Application/Interfaces/ITokenBlacklistService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface ITokenBlacklistService
+    {
+        Task AddTokenToBlacklist(string token);
+        Task<bool> IsTokenBlacklisted(string token);
+        Task RemoveTokenFromBlacklist(string token);
+    }
+
+    public class TokenBlacklistService : ITokenBlacklistService
+    {
+        private readonly HashSet<string> _blacklist = new HashSet<string>();
+
+        public Task AddTokenToBlacklist(string token)
+        {
+            _blacklist.Add(token);
+            return Task.CompletedTask;
+        }
+
+        public Task<bool> IsTokenBlacklisted(string token)
+        {
+            return Task.FromResult(_blacklist.Contains(token));
+        }
+
+        public Task RemoveTokenFromBlacklist(string token)
+        {
+            _blacklist.Remove(token);
+            return Task.CompletedTask;
+        }
+    }
+}
